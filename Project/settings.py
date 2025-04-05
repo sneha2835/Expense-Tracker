3c06e8167a4bf02711ee91a02e872432790a0e2c
sneha2835 <snehakamatam28@gmail.com>
2025-04-05 23:37:20 +0530

    New changes

--- Project/settings.py
+++ Project/settings.py
@@ -55,6 +55,12 @@ REST_FRAMEWORK = {
 
 AUTH_USER_MODEL = 'ExpBudApp.User'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React frontend in development
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
